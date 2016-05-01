4fe9169c168b88700132a1d01b07f050b98c644b
Ale1 <alejandro.barbero@gmail.com>
2016-05-01 16:26:51 -0300

    asdasd

---
+++ Webserver/WebServer.java
@@ -0,0 +1,149 @@
+
+import java.io.*;
+import java.util.Scanner;
+import java.net.*;
+import java.util.concurrent.*;
+import java.util.*;
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.ParseException;
+import org.json.simple.parser.JSONParser;
+ 
+
+class WebServer{
+
+    public static void main(String[] args) {
+        ServerSocket serverSocket;
+        int port_num = 5541;
+
+        try {
+            serverSocket = new ServerSocket(port_num); // create listening Server socket
+            System.out.println("Server started. Listening to the port: " + port_num);
+
+            while (true) {  // always on listening for TCP connection requests
+                
+                Socket clientSocket = serverSocket.accept(); // Always accept new clients
+                OutputStream outputStream = clientSocket.getOutputStream();
+                outputStream.write (("<html><body><p> YOUR MLA ITEMS</p><table><td>").getBytes());
+
+
+                //generate 10 requests with different offsets
+                int[] offsets = {1,2,3,4,5,6,7,8,9,10};
+                List<RequestHandler> threads = new ArrayList<RequestHandler>();
+
+             
+                for(int x : offsets){
+                    RequestHandler thread  = new RequestHandler(clientSocket, x);  // Make new thread for each request 
+                    thread.start();//and call it's run procedure
+                    threads.add( thread); // add to threads array, used later for join operation;
+                } 
+
+
+                // "join" makes it wait for all threads to complete.
+                for (RequestHandler thread : threads) {
+                  try{thread.join();} catch(Exception e) { System.out.println("join error");}
+                  
+                }
+
+                outputStream.write(
+                            ("</td></table><p> FINISHED BABY</p></body></html>").getBytes()
+                );    
+
+                           
+            } 
+
+        } catch (IOException e) {
+            System.out.println("Could not listen on port: " + port_num);
+        }      
+    }
+}
+
+   
+
+
+class RequestHandler extends Thread {
+    
+    Socket clientSocket;
+    int offset;
+
+    RequestHandler(Socket clientSocket,  int offset) {
+        this.clientSocket = clientSocket;
+        this.offset = offset;
+
+                    
+    }
+
+
+    public static String jsonParser(String text){
+    
+        JSONParser parser = new JSONParser();
+        String result = " error";
+        
+        try{
+            // parsing and fetching of permalink value in response JSON
+            // note: funciona pero es muy feo esto.  debe haber una forma mas directa.  
+            Object obj = parser.parse(text); 
+            JSONObject obj2 = (JSONObject)obj;
+            JSONArray obj3 = (JSONArray)obj2.get("results");
+            JSONObject obj4 = (JSONObject)obj3.get(0);
+            result = obj4.get("permalink").toString();
+      
+        }catch(ParseException pe){
+            System.out.println("Parse error");
+        }
+        return result;
+   }
+
+
+   public String parseInputStream(InputStream stream) {
+
+        String parsedStream;
+        try (Scanner scanner = new Scanner(stream)) {
+                parsedStream = scanner.useDelimiter("\\A").next();
+        }
+        return parsedStream;
+   }
+
+
+    public void run() {
+
+        System.out.println("new thread processing request on server (item: " + offset + ")");
+
+        try {
+                URL myURL = new URL("https://api.mercadolibre.com/sites/MLA/search?category=MLA1648&limit=1&offset="+offset+"&attributes=results");
+                HttpURLConnection connection = (HttpURLConnection)myURL.openConnection();
+                connection.setRequestMethod("GET");
+
+                connection.setRequestProperty("Content-Type", "application/json");
+                connection.setRequestProperty("Accept", "application/json");
+
+                InputStream inputStream = connection.getInputStream();
+                OutputStream outputStream = clientSocket.getOutputStream();
+
+                String responseBody = parseInputStream(inputStream); // inputstream to text
+                String response_url = jsonParser(responseBody);  // text to json, then fetch url
+
+           
+                try { 
+                    outputStream.write(("<tr><a href=" + response_url +">item "+ offset + "</a></tr>").getBytes());           
+                    System.out.println("Done processing request (item "+ offset +")");
+    
+                }catch(Exception e) {
+                    System.out.println("error processing request for item " + offset );
+                }
+
+        } 
+        catch (MalformedURLException e) { 
+                System.out.println("new URL() failed");
+        } 
+        catch (IOException e) {   
+                System.out.println("openConnection() failed");
+        }
+    }
+
+}
+
+   
+
+
