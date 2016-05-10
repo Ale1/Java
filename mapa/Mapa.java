3da2fb11f2a2a05d3e69aa1305b827896af86f3b
Ale1 <alejandro.barbero@gmail.com>
2016-05-10 14:02:29 -0300

    added-mapa-test

--- mapa/Mapa.java
+++ mapa/Mapa.java
@@ -7,32 +7,31 @@ import java.util.Arrays;
 
 class Mapa {
 
-	static ArrayList<String[]> pseudo_hash = new ArrayList<String[]>();  
+	static ArrayList<String[]> contents = new ArrayList<String[]>();  
 		
 	//adds new key-value, or edits existing one if already present
 	public static void put(String key , String value) {
 
-		for(int i = 0; i < pseudo_hash.size()+1; i++) {
-			
-		 //cycle through all keys. if key not found, add new key-value.
-			if (i == pseudo_hash.size()) {
+		for(int i = 0; i < contents.size()+1; i++) {
+		 	//cycle through all keys. if key not found, add new key-value.
+			if (i == contents.size()) {
 				String[] pair = new String[] {key, value}; 
-				pseudo_hash.add(pair);
+				contents.add(pair);
 				break;
 			}
 
 		//  else update value  if key already exists. 
-			if (pseudo_hash.get(i)[0] == key) {
-				pseudo_hash.get(i)[1] = value;
+			if (contents.get(i)[0] == key) {
+				contents.get(i)[1] = value;
 				break;
 			}
 		}
 	}
 
 	// fetches value for specified parameter key
-	public static String fetch(String key) {
-		for(int i = 0; i < pseudo_hash.size(); i++) {
-			String[] arr = pseudo_hash.get(i);
+	public static String get(String key) {
+		for(int i = 0; i < contents.size(); i++) {
+			String[] arr = contents.get(i);
 			if (arr[0] == key) {
 				return arr[1];
 			}	
@@ -47,13 +46,26 @@ class Mapa {
 class MapaTest  {
 	public static void main(String args[]) {
 		Mapa test = new Mapa();
-		System.out.println("empty:" + test.pseudo_hash);
+		System.out.println("empty:" + test.contents);
 		test.put ("name","juan");
 		test.put("surname", "perez");
-		System.out.println("nombre: " + test.fetch("name"));
-		System.out.println("apellido: " + test.fetch("surname"));
-		test.put ("name","tomas");
-		System.out.println("nuevo nombre: " + test.fetch("name"));
+		System.out.println("nombre: " + test.get("name"));
+		System.out.println("apellido: " + test.get("surname"));
+		test.put("name","tomas");
+		System.out.println("nuevo nombre: " + test.get("name"));
+		System.out.println("key inexistente: " + test.get("age"));
+		test.put("nationality","uruguay");
+
+		StringBuilder sb = new StringBuilder();
+		for (String[] a : test.contents)
+		{
+    		for (String s : a) 
+    		{	sb.append(s);
+    			sb.append(" ");
+    		}
+    		sb.append( " - ");
+		}
+		System.out.println("full hash: " + sb);
 
 	}
 
